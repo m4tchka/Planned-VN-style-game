37db34a5d8612310492a4d738af7df8e9c8fc9a8
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-07-05 22:40:05 +0100

    Working HandleClick function that cycles the dialogue file

--- vn/src/DialogueFile.js
+++ vn/src/DialogueFile.js
@@ -1,15 +1,30 @@
-let ch1 = [
+let ch1 = 
+[
     {
+        Background:"someImageFile.jpg",
         Scene1:[
             {   
-                Name: "Ganyu",
-                Dialogue:"I could do with an Adeptus' Temptation... ",
+                Name: "Person1",
+                Dialogue:"Greetings. My name is Person1 !",
             },
             {
-                Name:"Xiangling",
-                Dialogue:"Sounds delicious !"
-            }      
-        ]
+                Name:"Person2",
+                Dialogue:"The name's 2. Person 2."
+            },     
+            {
+                Name:"Person3",
+                Dialogue:"Howdy ! I'm Person3"
+            },
+            {
+                Name:"Person4",
+                Dialogue:"Hiyaa ~ People call me Person4, but I go by p4 ~"
+            },
+            {
+                Name:"Person1",
+                Dialogue:"Now you may be wondering why I have gathered you here today... "
+            },
+            
+        ],
     },
     {
         Scene2:[]
@@ -32,5 +47,15 @@ let ch2 = [
 /* 
 Animations: ["some","numbers","here"],
 Sprite: "Normal" 
+
+            {   
+                Name: "Ganyu",
+                Dialogue:"I could do with an Adeptus' Temptation... ",
+            },
+            {
+                Name:"Xiangling",
+                Dialogue:"Sounds delicious !"
+            },  
+
 */
 export { ch1, ch2 };
--- vn/src/components/App/App.js
+++ vn/src/components/App/App.js
@@ -1,14 +1,65 @@
+import { useState } from 'react';
 import "./App.css";
 import { ButtonList } from "../LowerSectionBox/ReusableButtonGroup/ButtonList/ButtonList.js";
 import { LowerSectionBox } from "../LowerSectionBox/LowerSectionBox.js" 
+import { ch1 } from '../../DialogueFile.js'
 function App() {
+    const [sceneArrayEntry,setSceneArrayEntry,] = useState(0)
+    const [currentName, setCurrentName] = useState("");
+    const [currentDialogue, setCurrentDialogue] = useState ("");
+
+    function switchDialogue () {
+        setCurrentDialogue(ch1[0].Scene1[sceneArrayEntry].Dialogue)
+    };
+    function switchName () {
+        setCurrentName(ch1[0].Scene1[sceneArrayEntry].Name)
+        console.log(ch1[0].Scene1[sceneArrayEntry].Name);
+    };
+
+    function handleClick () {
+       
+        if (sceneArrayEntry===ch1[0].Scene1.length-1) {
+            setSceneArrayEntry(0);
+            switchName();
+            switchDialogue();
+        }
+        else {
+            setSceneArrayEntry(sceneArrayEntry+1)
+            switchName();
+            switchDialogue();
+        };
+    }
+
+
+
+/* >>> Wrap both onClick functions in a wrapping function <<< */
+
     return (
         <div className="App">
-            <LowerSectionBox 
-                CharacterName={"Ganyu"}
-                Dialogue={'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."'}
-                ButtonList={ButtonList}/>
+        <p> Hello </p>
+                <p onClick={handleClick}>Hello {currentName}</p>
+                <p>{currentDialogue}</p>
         </div>
     );
 };
 export default App;
+
+/*    
+    const [currentName, setCurrentName] = useState("");
+    const [currentDialogue, setCurrentDialogue] = useState ("");
+
+    function switchDialogue () {
+        setCurrentDialogue()
+    };
+    function switchName () {
+        setCurrentName()
+    }; 
+
+<LowerSectionBox 
+                onClick={ switchDialogue + switchName }
+                CharacterName={ currentName }
+                Dialogue={ currentDialogue }
+                ButtonList={ ButtonList }/>
+
+
+*/
