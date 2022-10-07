f92c4124eb8350a74e75d831e69cb69d490e5ebf
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-07 22:50:36 +0100

    Start button correctly links to start of story

--- vn/src/components/MainMenu/MainMenu.js
+++ vn/src/components/MainMenu/MainMenu.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 export default function MainMenu() {
     // TODO: React Router DOM
     // To make bg full screen, take from top-level div in App.js
     return (
         <div className="MainMenuPage">
-            <h1 className="Title"> --- Title goes here --- </h1>
-            <div className="VerticalButtonBar">
-                <button>Start</button>
+            <h1 className="Title"> --- Title --- </h1>
+            <div className="vertical-ribbon">
+                <Link to="/story">
+                    <button>Start</button>
+                </Link>
                 <button>Load</button>
                 <button>Options</button>
                 <button>About</button>
