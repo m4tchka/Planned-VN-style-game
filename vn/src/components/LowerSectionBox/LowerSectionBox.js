fd66b4347ad92ca057dcadbb8376a0cb4fa41bfe
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-12 22:48:14 +0000

    remove textbox, the final nested component in file structure

--- vn/src/components/LowerSectionBox/LowerSectionBox.js
+++ vn/src/components/LowerSectionBox/LowerSectionBox.js
@@ -1,16 +1,23 @@
-import "./LowerSectionBox.css"
-import { Textbox } from "./Textbox/Textbox.js";
+import "./LowerSectionBox.css";
 
-function LowerSectionBox ( { CharacterName, Dialogue, onClick } ) {
-
+function LowerSectionBox({ CharacterName, Dialogue, onClick }) {
     return (
         <section className="LowerSectionBox">
-            <Textbox
-                onClick={onClick} 
-                CharacterName={CharacterName}
-                Dialogue={Dialogue}
-            />
+            <div
+                className="textbox"
+                id="clickable-dialogue-box"
+                onClick={onClick}
+            >
+                <div className="name-box">
+                    <h3 className="name-bar" id={CharacterName}>
+                        {CharacterName}
+                    </h3>
+                </div>
+                <div className="dialogue-box">
+                    <p>{Dialogue}</p>
+                </div>
+            </div>
         </section>
     );
-};
+}
 export { LowerSectionBox };
