35b53e21f82af857aea188b1c64a0094f6e97a1a
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-02-18 19:50:54 +0000

    Add test dialogue and structure

--- vn/src/dialogueFileTest.js
+++ vn/src/dialogueFileTest.js
@@ -1,48 +1,101 @@
-let ch1 = {
-	0:{
+let ch1Test = {
+    0: {
         scene: [
-            {},
-			{},
-			{
-                Name: "",
-                Dialogue: "",
-                Question: "Where should we go ?",
+            {
+                Name: "Test Name",
+                Dialogue: "Test Dialogue",
+                Background: `/backgrounds/test.jpg`,
+            },
+            {
+                Name: "Test Name2",
+                Dialogue: "Test Dialogue2",
+            },
+            {
+                Name: "Test Name2",
+                Dialogue: "Test Dialogue2",
+                Question: "Test question ??",
                 Options: [
                     {
-                        Text: "Visit the densely forested valley",
+                        Text: "Option 1",
                         Next: 1,
                         LuckChange: +1,
                         MinLuck: 0,
                     },
                     {
-                        Text: "Take the meandering path along the cliffside",
+                        Text: "Option 2",
                         Next: 2,
-                        LuckChange: +2,
+                        LuckChange: +1,
                         MinLuck: 0,
                     },
                     {
-                        Text: "Follow the road straight through the forest",
+                        Text: "Option 3",
                         Next: 3,
-                        LuckChange: -1,
+                        LuckChange: +1,
                         MinLuck: 0,
                     },
                 ],
-            }
-		]
-	},
-	1:{
-		scene: [
+            },
+        ],
+    },
+    1: {
+        scene: [
             {},
-			{},
-			{
-				Name: "",
+            {},
+            {
+                Name: "",
                 Dialogue: "",
-				Options: [
-					{},
-					{},
-					{},
-				],
-			},
-		],
-	}
-}
+                Options: [
+                    {
+                        Text: "Option 1",
+                        Next: 4,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                    {
+                        Text: "Option 2",
+                        Next: 5,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                    {
+                        Text: "Option 3",
+                        Next: 6,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                ],
+            },
+        ],
+    },
+    2: {
+        scene: [
+            {},
+            {},
+            {
+                Name: "",
+                Dialogue: "",
+                Options: [
+                    {
+                        Text: "Option 1 leading to 4",
+                        Next: 4,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                    {
+                        Text: "Option 2 leading to 5",
+                        Next: 5,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                    {
+                        Text: "Option 3 leading to 6",
+                        Next: 6,
+                        LuckChange: +1,
+                        MinLuck: 0,
+                    },
+                ],
+            },
+        ],
+    },
+};
+export { ch1Test };
